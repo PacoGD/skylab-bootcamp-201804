1f52937e52e7f4852b13d2186e52402d3cf4e2f3
Your Name <you@example.com>
2018-06-18 10:42:08 +0200

    update bootstrap

--- staff/PacoGD/furniture-app/client/app/furnitureapp/src/App.js
+++ staff/PacoGD/furniture-app/client/app/furnitureapp/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Home from './components/Home/Home.js'
+import Footer from './components/Footer';
+import './vendor/bootstrap/4.1.0/css/bootstrap.min.css'
+
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    url: '',
+    token: '',
+    username: '',
+    password: '',
+    id: ''
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        
+        <Header/>
+        <Home/>
+        <Footer />
+
       </div>
     );
   }
---
+++ staff/PacoGD/furniture-app/client/app/furnitureapp/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+// import logic from '../logic'
+function Header() {
+    return (
+        <header>
+            <p>Header</p>
+        </header>
+    )
+}
+
+export default Header
